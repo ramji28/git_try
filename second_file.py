9b61b4f6af08cabd75fbcaa05d740a5577c0500c
RamPrakash <ramprakash2895@gmail.com>
2023-06-03 01:15:21 +0530

    pleading

--- second_file.py
+++ second_file.py
@@ -1,4 +1,5 @@
 print("Hello thsi is the second file I am creating")
 print("this from a new branch called new_features")
 print("this is not merging y?")
-print("a line with error") # i have correcet the error in the new_features branch
+print("a line with error")
+print("please show the merge conflict") # i have correcet the error in the new_features branch
