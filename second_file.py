ce4008b3dd6e3a27ca2ae368ef63166039e2ade4
RamPrakash <ramprakash2895@gmail.com>
2023-06-03 01:04:12 +0530

    correct the error in second_file.py

--- second_file.py
+++ second_file.py
@@ -1,4 +1,4 @@
 print("Hello thsi is the second file I am creating")
 print("this from a new branch called new_features")
 print("this is not merging y?")
-print("a line with error)
+print("a line with error") # i have correcet the error in the new_features branch
